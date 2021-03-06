5a778004fb7c08ffd4e4bdacd75d2e68fe0a66a4
Alex Amorim <obr.alexalexamorim@gmail.com>
2021-03-06 01:04:43 -0300

    Projeto hr-worker criado

---
+++ hr-worker/src/test/java/com/local/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
