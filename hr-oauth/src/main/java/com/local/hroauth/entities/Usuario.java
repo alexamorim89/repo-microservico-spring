327f91a165330ef99a49322d45c3e49baaaeee8e
Alex Amorim <obr.alexalexamorim@gmail.com>
2021-03-11 23:03:29 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/local/hroauth/entities/Usuario.java
+++ hr-oauth/src/main/java/com/local/hroauth/entities/Usuario.java
@@ -1,10 +1,16 @@
 package com.local.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class Usuario implements Serializable {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class Usuario implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
@@ -73,6 +79,42 @@ public class Usuario implements Serializable {
 	}
 
 	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -87,6 +129,5 @@ public class Usuario implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
+	}	
 }
--- hr-oauth/src/main/java/com/local/hroauth/services/UsuarioService.java
+++ hr-oauth/src/main/java/com/local/hroauth/services/UsuarioService.java
@@ -3,26 +3,42 @@ package com.local.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.local.hroauth.entities.Usuario;
 import com.local.hroauth.feignclients.UsuarioFeignClient;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UsuarioService.class);
 	
 	@Autowired
 	private UsuarioFeignClient usuarioFeignClient;
 	
+	
 	public Usuario findByEmail(String email) {
 		Usuario usuario = this.usuarioFeignClient.findByEmail(email).getBody();
 		if(usuario == null) {
-			logger.error("Email not found: " + email);
+			logger.error("Email nao encontrado: " + email);
 			throw new IllegalArgumentException("Email nao encontrado");
 		}
 		logger.info("Email encontrado: " + email);
+		return usuario;
+	}
+	
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = this.usuarioFeignClient.findByEmail(username).getBody();
+		if(usuario == null) {
+			logger.error("Email nao encontrado: " + username);
+			throw new UsernameNotFoundException("Email nao encontrado");
+		}
+		logger.info("Email encontrado: " + username);
 		return usuario;
 	}
 	
