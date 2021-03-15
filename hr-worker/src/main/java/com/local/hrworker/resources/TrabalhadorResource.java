fc3d0d507d2036db570ab96c466f3cad89c59b1a
Alex Amorim <obr.alexalexamorim@gmail.com>
2021-03-15 16:46:51 -0300

    db config server

--- hr-worker/src/main/java/com/local/hrworker/resources/TrabalhadorResource.java
+++ hr-worker/src/main/java/com/local/hrworker/resources/TrabalhadorResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +23,8 @@ public class TrabalhadorResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(TrabalhadorResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
+	//@Value("${test.config}")
+	//private String testConfig;
 
 	@Autowired
 	private Environment env;
@@ -59,7 +58,7 @@ public class TrabalhadorResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
